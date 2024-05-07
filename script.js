d6bfcbb84cd1cf8c12855feb20145c1ed54eda01
CristianCMB <cristian.combei@student.upt.ro>
2024-05-07 21:41:00 +0300

    login-page done

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const wrapper=document.querySelector('.wrapper');
-const loginLink=document.querySelector('.login-link');
-const registerLink=document.querySelector('.register-link');
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 
-registerLink.addEventListener('click', ()=>
-{
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.parentElement.querySelector(".form__input-error-message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 })
 
-loginLink.addEventListener('click', ()=>
-{
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 })
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('uname').value;
+    var password = document.getElementById('psw').value;
+
+   if(username === 'admin' && password === 'parola') {
+    Swal.fire({
+        icon: 'success',
+        title: 'Login successful',
+        showConfirmButton: false,
+        timer: 1500
+    });
+    setTimeout(function() {
+            window.location.href = 'home.html';
+        }, 2000);
+} else {
+    setFormMessage(this, "error", "Invalid username/password combination");
+}
+});
