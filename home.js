17394bfd067953d1443a2cd8ea1db2c02071a1df
CristianCMB <cristian.combei@student.upt.ro>
2024-05-13 03:44:07 +0300

    finished..for now

--- home.js
+++ home.js
@@ -17,29 +17,31 @@ for (let i=0; i<infoBoxes.length; i++){
 
 // slides
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1,1,1,1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  console.log("test");
+  showSlides(slideIndex[no] += n, no);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
+  console.log(slideId[no]);
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    console.log(x[i]);
+    x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  if(x[slideIndex[no]-1] != undefined){
+ 
+  x[slideIndex[no]-1].style.display = "block";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 } 
